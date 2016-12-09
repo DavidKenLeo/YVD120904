0c06c3ac67c8309fafeb7c63bff6253642ed8aa2
Shaojung <shaojung@gmail.com>
2016-12-09 13:27:17 +0800

    Fragment Transation

--- app/src/main/java/com/example/user/yvd120904/MainActivity.java
+++ app/src/main/java/com/example/user/yvd120904/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.yvd120904;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        android.app.FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.linearLayout1, new FragmentA(), "f_a");
+        ft.commit();
+    }
+
+    public void click2(View v)
+    {
+        android.app.FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.linearLayout1, new FragmentB(), "f_b");
+        ft.commit();
     }
 }
